5f46e0652a2e913203433c80202b4833b15a891d
muthash <kathingo92@gmail.com>
2018-03-30 22:39:47 +0300

    test user login

--- tests/test_user.py
+++ tests/test_user.py
@@ -8,6 +8,26 @@ class AuthTestCase(BaseTestCase):
     def test_registration(self):
         """Test user registration works correcty."""
         res = self.register_user()
+        self.assertTrue(res.content_type == 'application/json')
         result = json.loads(res.data.decode())
-        self.assertEqual(result['message'], "You registered successfully")
+        self.assertEqual(result['message'], "Account created successfully")
         self.assertEqual(res.status_code, 201)
+
+    def test_already_registered_user(self):
+        """Test that a user cannot be registered twice."""
+        self.register_user()
+        second_res = self.register_user()
+        self.assertTrue(second_res.content_type == 'application/json')
+        result = json.loads(second_res.data.decode())
+        self.assertEqual(result['message'], "User already exists")
+        self.assertEqual(second_res.status_code, 409)
+
+    def test_user_login(self):
+        """Test registered user can login."""
+        self.register_user()
+        res = self.login_user()
+        self.assertTrue(res.content_type == 'application/json')
+        result = json.loads(res.data.decode())
+        self.assertEqual(result['message'], "Login successfull")
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(result['access_token'])
