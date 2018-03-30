55bcbd7ffa2ba0cf44fb0c0eda50448e83add177
muthash <kathingo92@gmail.com>
2018-03-30 21:09:37 +0300

    Add empty input validation check

---
+++ app/utils.py
@@ -0,0 +1,29 @@
+import re
+
+
+def validate_email(email):
+    """This function is used to validate a user's email"""
+    if len(email) > 4:
+        regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$)"
+        if re.match(regex, email) is not None:
+            return True
+    return False
+
+def validate_null(**kwargs):
+    """This function is used to validate a null input"""
+    messages = []
+    for key in kwargs:
+        if kwargs[key] is None:
+            message = 'Please enter your {}'.format(key)
+            messages.append(message)
+    return messages
+
+def validate_empty_text(**kwargs):
+    """This function is used to validate whitespace input"""
+    messages = []
+    for key in kwargs:
+        strip_text = re.sub(r'\s+', '', kwargs[key])
+        if not strip_text:
+            message = 'Please enter your {}'.format(key)
+            messages.append(message)
+    return messages
