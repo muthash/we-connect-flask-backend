1be1fd1d30c2a137a84cb7fe9089598a0e346d72
muthash <kathingo92@gmail.com>
2018-03-30 14:19:43 +0300

    add registration functionality

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,4 +3,29 @@ from flask import Blueprint, request, jsonify
 from flask.views import MethodView
 from app.models import User
 
-auth = Blueprint('auth', __name__, url_prefix='/api/v1/auth')
+auth = Blueprint('auth', __name__, url_prefix='/api/v1')
+
+
+class RegisterUser(MethodView):
+    """
+    This Method view is used to Register a new user
+    using the POST method
+    """
+    def post(self):
+        """Endpoint used to save the data to the database"""
+        if not request.get_json():
+            response = {'error':'Bad Request. Request should be JSON format'}
+            return jsonify(response), 400
+        data = request.get_json()
+        email = data.get('email')
+        username = data.get('username')
+        password = data.get('password')
+        user = user = User.query.filter_by(email=email).first()
+        if not user:
+            user = User(email=email, username=username, password=password)
+            user.save()
+            response = {
+                'message': 'You registered successfully. Please log in.'
+            }
+            return jsonify(response), 201
+auth.add_url_rule('/register', view_func=RegisterUser.as_view('register'))
