773969865c1c9873b6078f78a39a437fc98f55db
muthash <kathingo92@gmail.com>
2018-03-30 15:18:00 +0300

    validate email input

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,6 +2,7 @@
 from flask import Blueprint, request, jsonify
 from flask.views import MethodView
 from app.models import User
+from app.utils import validate_email,
 
 auth = Blueprint('auth', __name__, url_prefix='/api/v1')
 
@@ -20,12 +21,17 @@ class RegisterUser(MethodView):
         email = data.get('email')
         username = data.get('username')
         password = data.get('password')
-        user = user = User.query.filter_by(email=email).first()
-        if not user:
-            user = User(email=email, username=username, password=password)
-            user.save()
-            response = {
-                'message': 'You registered successfully. Please log in.'
-            }
-            return jsonify(response), 201
+
+        
+        if validate_email(email):
+            user = user = User.query.filter_by(email=email).first()
+            if not user:
+                user = User(email=email, username=username, password=password)
+                user.save()
+                response = {
+                    'message': 'You registered successfully. Please log in.'
+                }
+                return jsonify(response), 201
+
+
 auth.add_url_rule('/register', view_func=RegisterUser.as_view('register'))
