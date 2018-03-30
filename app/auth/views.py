69063c6f5388a7abbd056d13996868a82659daa6
muthash <kathingo92@gmail.com>
2018-03-30 23:03:15 +0300

    add login functionality

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,12 +8,9 @@ auth = Blueprint('auth', __name__, url_prefix='/api/v1')
 
 
 class RegisterUser(MethodView):
-    """
-    This Method view is used to Register a new user
-    using the POST method
-    """
+    """Method to Register a new user"""
     def post(self):
-        """Endpoint used to save the data to the database"""
+        """Endpoint to save the data to the database"""
         if not request.get_json():
             response = {'error':'Bad Request. Request should be JSON format'}
             return jsonify(response), 400
@@ -32,10 +29,36 @@ class RegisterUser(MethodView):
             if not user:
                 user = User(email=email, username=username, password=password)
                 user.save()
-                response = {
-                    'message': 'You registered successfully'
-                }
+                response = {'message': 'Account created successfully'}
                 return jsonify(response), 201
+            response = {'message': 'User already exists'}
+            return jsonify(response), 409
+        response = {'message': 'Please enter a valid email address'}
+        return jsonify(response), 400
+
+
+class LoginUser(MethodView):
+    """Method to login a new user"""
+    def post(self):
+        """Endpoint to login a user"""
+        if not request.get_json():
+            response = {'error':'Bad Request. Request should be JSON format'}
+            return jsonify(response), 400
+        data = request.get_json()
+        email = data.get('email')
+        password = data.get('password')
+
+        null_input = validate_null(email=email, password=password)
+        if null_input:
+            response = {'message': null_input}
+            return jsonify(response), 400
+
+        if validate_email(email):
+            user = user = User.query.filter_by(email=email).first()
+            if user and user.password_is_valid(password):
+                response = {'message': 'Login successfull'}
+                return jsonify(response), 200
 
 
 auth.add_url_rule('/register', view_func=RegisterUser.as_view('register'))
+auth.add_url_rule('/login', view_func=LoginUser.as_view('login'))
