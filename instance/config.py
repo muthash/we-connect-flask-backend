56b720ee1f6ad34753c793780dd2ecefac2f6124
muthash <kathingo92@gmail.com>
2018-03-31 00:00:20 +0300

    enable token blacklisting

--- instance/config.py
+++ instance/config.py
@@ -19,7 +19,7 @@ class Config(object):
         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    JWT_BLACKLIST_ENABLED = True
 
 class DevelopmentConfig(Config):
     """Configurations for Development"""
