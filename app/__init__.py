012cc3aa891e6e0fed12a09a9cd91ae829d0f6fd
muthash <kathingo92@gmail.com>
2018-03-31 00:03:20 +0300

    Initialize flask jwt extended

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,15 @@
     returns it after it's loaded up with configuration settings
     using app.config
 """
-
+from flask import jsonify
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 from instance.config import app_config
 
 
 db = SQLAlchemy()
+jwt = JWTManager()
 
 
 def create_app(config_name):
@@ -19,8 +21,15 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    jwt.init_app(app)
 
     from app.auth.views import auth
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        """Error handler for wrong method to an endpoint"""
+        return jsonify({'message':'Method not allowed'}), 405
+
     app.register_blueprint(auth)
 
     return app
