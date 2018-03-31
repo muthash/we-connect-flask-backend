e6ac7dd1d1734179a79a396da99a5f9ab7f5cbca
muthash <kathingo92@gmail.com>
2018-03-31 19:05:33 +0300

    Add make request function

--- tests/test_base.py
+++ tests/test_base.py
@@ -13,34 +13,37 @@ class BaseTestCase(unittest.TestCase):
         """Set up test variables"""
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client()
+        self.url = None
+        self.data = None
+        self.header = {'Content-Type': 'application/json'}
 
         with self.app.app_context():
             db.session.close()
             db.drop_all()
             db.create_all()
 
+    def make_request(self, url, method='post', **kwargs):
+        """This helper method makes a post request"""
+        self.url = str(url)
+        self.data = json.dumps(kwargs['data'])
+        if method == 'put':
+            return self.client.put(path=self.url, headers=self.header, data=self.data)
+        return self.client.post(path=self.url, headers=self.header, data=self.data)
+
     def register_user(self, email="user@test.com", username="stephen",
                       password="test1234"):
-        """This helper method helps register a test user."""
+        """This helper method helps register a test user"""
         user_data = {'email': email, 'username': username,
                      'password': password}
-        return self.client.post(
-            '/api/v1/register',
-            headers={'Content-Type': 'application/json'},
-            data=json.dumps(user_data)
-            )
+        return self.make_request('/api/v1/register', data=user_data)
 
     def login_user(self, email="user@test.com", password="test1234"):
-        """This helper method helps log in a test user."""
+        """This helper method helps log in a test user"""
         user_data = {'email': email, 'password': password}
-        return self.client.post(
-            '/api/v1/login',
-            headers={'Content-Type': 'application/json'},
-            data=json.dumps(user_data)
-            )
+        return self.make_request('/api/v1/login', data=user_data)
 
     def tearDown(self):
-        """teardown all initialized variables."""
+        """teardown all initialized variables"""
         with self.app.app_context():
             db.session.remove()
             db.drop_all()
