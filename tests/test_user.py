1e8375956213f464006eefa5f2c90dfe8b381c6d
muthash <kathingo92@gmail.com>
2018-04-01 22:00:26 +0300

    Test password change

--- tests/test_user.py
+++ tests/test_user.py
@@ -113,49 +113,82 @@ from tests.test_base import BaseTestCase
 #         self.assertEqual(result['msg'], "Token has been revoked")
 #         self.assertEqual(logout_res.status_code, 401)
 
-class TestResetPassword(BaseTestCase):
-    """Test reset password user endpoint"""
-    def test_password_reset(self):
-        """Test password reset"""
-        self.register_user()
-        reset_res = self.make_request('/api/v1/reset-password', data=dict(email='user@test.com'))
-        result = json.loads(reset_res.data.decode())
-        self.assertEqual(result['message'], "An email has been sent with your new password")
-        self.assertEqual(reset_res.status_code, 201)
 
-    def test_json_request(self):
-        """Test reset password request for valid json"""
-        res = self.client.post('/api/v1/reset-password', data=dict(email='user@test.com'))
-        self.assertEqual(res.status_code, 405)
-        result = json.loads(res.data.decode())
-        self.assertEqual(result['message'], 'Bad Request. Request should be JSON format')
-
-    def test_reset_non_existing_email(self):
-        """Test reset password with a non existing account"""
-        reset_res = self.make_request('/api/v1/reset-password', data=dict(email='none@test.com'))
-        result = json.loads(reset_res.data.decode())
-        self.assertEqual(result['message'], "Please enter a valid email address")
-        self.assertEqual(reset_res.status_code, 401)
-
-    def test_reset_invalid_email(self):
-        """Test reset password with an invalid email address"""
-        reset_res = self.make_request('/api/v1/reset-password', data=dict(email='usertest'))
-        result = json.loads(reset_res.data.decode())
-        self.assertEqual(result['message'], "Please enter a valid email address")
-        self.assertEqual(reset_res.status_code, 400)
-
-
-#     def test_password_change(self):
-#         """Test password change"""
+# class TestResetPassword(BaseTestCase):
+#     """Test reset password user endpoint"""
+#     def test_password_reset(self):
+#         """Test password reset"""
 #         self.register_user()
 #         reset_res = self.make_request('/api/v1/reset-password', data=dict(email='user@test.com'))
 #         result = json.loads(reset_res.data.decode())
-#         passwords = {'old_password': result['password'], 'new_password': 'newtestpass'}
-#         self.get_login_token(email="user@test.com", password=result['password'])
-#         change_res = self.make_request('/api/v1/change-password', data=passwords, method='put')
-#         result = json.loads(change_res.data.decode())
-#         self.assertEqual(result['message'], "Password changed successfully")
-#         self.assertEqual(change_res.status_code, 200)
+#         self.assertEqual(result['message'], "An email has been sent with your new password")
+#         self.assertEqual(reset_res.status_code, 201)
+
+#     def test_json_request(self):
+#         """Test reset password request for valid json"""
+#         res = self.client.post('/api/v1/reset-password', data=dict(email='user@test.com'))
+#         self.assertEqual(res.status_code, 405)
+#         result = json.loads(res.data.decode())
+#         self.assertEqual(result['message'], 'Bad Request. Request should be JSON format')
+
+#     def test_reset_non_existing_email(self):
+#         """Test reset password with a non existing account"""
+#         reset_res = self.make_request('/api/v1/reset-password', data=dict(email='none@test.com'))
+#         result = json.loads(reset_res.data.decode())
+#         self.assertEqual(result['message'], "Please enter a valid email address")
+#         self.assertEqual(reset_res.status_code, 401)
+
+#     def test_reset_invalid_email(self):
+#         """Test reset password with an invalid email address"""
+#         reset_res = self.make_request('/api/v1/reset-password', data=dict(email='usertest'))
+#         result = json.loads(reset_res.data.decode())
+#         self.assertEqual(result['message'], "Please enter a valid email address")
+#         self.assertEqual(reset_res.status_code, 400)
+
+
+class TestChangetPassword(BaseTestCase):
+    """Test change password user endpoint"""
+    def test_password_change(self):
+        """Test password change"""
+        passwords = {'old_password': "test1234", 'new_password': "newtestpass"}
+        self.get_login_token()
+        change_res = self.make_request('/api/v1/change-password', data=passwords, method='put')
+        result = json.loads(change_res.data.decode())
+        self.assertEqual(result['message'], "Password changed successfully")
+        self.assertEqual(change_res.status_code, 200)
+
+    def test_incorrect_initial_password(self):
+        """Test password with incorrect old password input"""
+        passwords = {'old_password': "test123", 'new_password': "newtestpass"}
+        self.get_login_token()
+        change_res = self.make_request('/api/v1/change-password', data=passwords, method='put')
+        result = json.loads(change_res.data.decode())
+        self.assertEqual(result['message'], "Invalid password")
+        self.assertEqual(change_res.status_code, 401)
+
+    def test_json_request(self):
+        """Test change password request for valid json"""
+        passwords = {'old_password': "test123", 'new_password': "newtestpass"}
+        self.register_user()
+        login_res = self.login_user()
+        result = json.loads(login_res.data.decode())
+        self.header['Authorization'] = 'Bearer ' + result['access_token']
+        change_res = self.client.put(
+            '/api/v1/change-password',
+            headers={'Authorization': 'Bearer ' + result['access_token']},
+            data=passwords)
+        result = json.loads(change_res.data.decode())
+        self.assertEqual(result['message'], "Bad Request. Request should be JSON format")
+        self.assertEqual(change_res.status_code, 405)
+
+    def test_invalid_initial_password(self):
+        """Test password with incorrect old password input"""
+        passwords = {'old_password': "test123", 'new_password': "  "}
+        self.get_login_token()
+        change_res = self.make_request('/api/v1/change-password', data=passwords, method='put')
+        result = json.loads(change_res.data.decode())
+        self.assertEqual(result['message'], ['Please enter your new_password'])
+        self.assertEqual(change_res.status_code, 400)
 
 #     def test_delete_account(self):
 #         """Test deleting account"""
