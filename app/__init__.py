4bd57262cdddf39b542f276f4f1d50e2ba64827b
muthash <kathingo92@gmail.com>
2018-04-02 19:14:45 +0300

    Add default 404 error message

--- app/__init__.py
+++ app/__init__.py
@@ -34,17 +34,22 @@ def create_app(config_name):
     @app.errorhandler(400)
     def bad_request(error):
         """Error handler for a bad request"""
-        return jsonify({'message':'The Server did not understand the request'}), 400
+        return jsonify({error:'The Server did not understand the request'}), 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        """Error handler for not found page"""
+        return jsonify({error:'Resource not found'}), 404
 
     @app.errorhandler(405)
     def method_not_allowed(error):
         """Error handler for wrong method to an endpoint"""
-        return jsonify({'message':'Method not allowed'}), 405
+        return jsonify({error:'Method not allowed'}), 405
 
     @app.errorhandler(500)
     def server_error(error):
         """Error handler for wrong method to an endpoint"""
-        return jsonify({'message':'Database connection failed! Try Again'}), 500
+        return jsonify({error:'Database connection failed! Try Again'}), 500
 
     @jwt.token_in_blacklist_loader
     def check_if_token_in_blacklist(decrypted_token):
