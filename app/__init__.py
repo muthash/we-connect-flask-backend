8316e7a5475c039f6257ad18b90ff36c6467f284
Muthash <stephen.kathingo@andela.com>
2018-05-16 20:49:07 +0300

    Add error handlers

--- app/__init__.py
+++ app/__init__.py
@@ -28,6 +28,36 @@ def create_app(config_name):
 
     from app.auth.views import auth
 
+    @app.errorhandler(400)
+    def bad_request(error):
+        """Error handler for a bad request"""
+        return jsonify(dict(error='The Server did not understand' +
+                                  'the request')), 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        """Error handler for not found page"""
+        return jsonify(dict(error='The Resource is not available')), 404
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        """Error handler for wrong method to an endpoint"""
+        return jsonify(dict(error='The HTTP request Method' +
+                                  ' is not allowed')), 405
+
+    @app.errorhandler(500)
+    def server_error(error):
+        """Error handler for a server failure"""
+        return jsonify(dict(error='The server encountered an internal error' +
+                                  ' and was unable to' +
+                                  ' complete your request')), 500
+
+    @jwt.token_in_blacklist_loader
+    def check_if_token_in_blacklist(decrypted_token):
+        """Check if token is blaclisted before allowing access to a route"""
+        jti = decrypted_token['jti']
+        # return jti in blacklist
+
 
     app.register_blueprint(auth)
 
