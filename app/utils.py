bb77649fc614de84a9f331f550a00cb42b2e5b75
Muthash <stephen.kathingo@andela.com>
2018-05-16 15:11:17 +0300

    Add validation function

--- app/utils.py
+++ app/utils.py
@@ -1,30 +1,85 @@
 import re, uuid
 
+from flask import request, jsonify
+from functools import wraps
+from usernames import is_safe_username
+
 from smtplib import SMTPException
 from flask_mail import Message
+from email_validator import validate_email, EmailNotValidError
 from app import mail
 
 
-def validate_email(email):
-    """Returns true if email is valid email format else false"""
-    regex = r"(^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$)"
-    if len(email) < 5 or re.match(regex, email) is None:
-        return False
-    return True
+def require_json(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if request.get_json(silent=True) is None:
+            response = {'message': 'The Request should be JSON format'}
+            return jsonify(response), 422
+        return func(*args, **kwargs)
+    return wrapper
 
-def validate_null(**kwargs):
-    """Returns a list with invalid field messages"""
-    messages = []
+def check_email(email):
+    try:
+        validate_email(email, check_deliverability=False)
+        return False
+    except EmailNotValidError as error:
+        response = {'message': str(error)}
+        return jsonify(response), 400
+
+def normalise_email(email):
+    validator_response = validate_email(email, check_deliverability=False)
+    email = validator_response["email"]
+    return email
+
+def check_username(username):
+    regex = re.compile('^[a-zA-Z0-9_]{4,}$')
+    res = re.match(regex, str(username))
+    if not is_safe_username(username):
+        response = {'message': "The username you provided is not allowed, " +
+                               "please try again but with a different name."}
+        return jsonify(response), 400
+    if not res:
+        response = {'message': "The Username should contain atleast four alpha-numeric characters. " +
+                               "The optional special character allowed is _ (underscore)."}
+        return jsonify(response), 400
+
+def check_password(password):
+    if re.match(r'[a-zA-Z_]+[A-Za-z0-9@#$%^&+=]{8,}', str(password)):
+        return False
+    response = {'message': 'Password should contain at least eight ' +
+                            'characters with at least one digit, one ' +
+                            'uppercase letter and one lowercase letter'}
+    return jsonify(response), 400
+    
+
+def check_missing_field(**kwargs):
+    errors={}
     for key in kwargs:
         if kwargs[key] is None:
-            message = 'Please enter your {}'.format(key)
-            messages.append(message)
+            error_key = key + '-error'
+            errors[error_key] = f'The {key} should not be missing'
         else:
-            strip_text = re.sub(r'\s+', '', kwargs[key])
+            strip_text = re.sub(r'\s+', '', str(kwargs[key]))
             if not strip_text:
-                message = 'Please enter your {}'.format(key)
-                messages.append(message)
-    return messages
+                error_key = key + '-error'
+                errors[error_key] = f'The {key} should not be empty'
+    return errors
+
+def validate_registration(email, username, password):
+    email = str(email)
+    username = str(username)
+    password = str(password)
+    if check_email(email):
+        return check_email(email)
+    if check_username(username):
+        return check_username(username)
+    if check_password(password):
+        return check_password(password)
+    return False
+
+
+
 
 def random_string(string_length=8):
     """Returns a random string of length string_length"""
