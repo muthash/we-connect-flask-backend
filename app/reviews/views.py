ccf492426a2d273629c4596293d8033d4a24ead7
Muthash <stephen.kathingo@andela.com>
2018-05-17 13:11:45 +0300

    Add review manipulation endpoints

---
+++ app/reviews/views.py
@@ -0,0 +1,47 @@
+""""Contains views to register, login reset password and logout user"""
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity, jwt_optional
+from app.models import User,  Business, Review
+from app.utils import require_json, check_missing_field, messages, remove_more_spaces
+from app.base_view import BaseView
+
+rev = Blueprint('rev', __name__,
+                url_prefix='/api/v1/businesses/<int:business_id>/reviews')
+POSTS_PER_PAGE = 2
+
+
+class ReviewManipulation(BaseView):
+    """Method to manipulate business endpoints"""
+    @jwt_required
+    @require_json
+    def post(self, business_id):
+        """Endpoint to save the data to the database"""
+        data = request.get_json()
+        description = data.get('description')
+        rating = data.get('rating')
+        current_user = get_jwt_identity()
+
+        data = dict(description=description, rating=rating)
+        if check_missing_field(**data):
+            return jsonify(check_missing_field(**data)), 422
+
+        user = User.query.filter_by(id=current_user).first()
+        description = remove_more_spaces(description)
+        business = Business.query.filter_by(id=business_id).first()
+
+        if not user:
+            return self.generate_response(messages['valid_login'], 403)
+        if business.user_id == current_user:
+            response = {'message': 'The operation is forbidden for' +
+                                   ' own business'}
+            return jsonify(response), 403
+
+        review = Review(description, rating, business_id, current_user)
+        review.save()
+        response = {'message': 'Review for business with id' +
+                               f' {business_id} created'}
+        return jsonify(response), 201
+
+
+review_view = ReviewManipulation.as_view('reviews')
+rev.add_url_rule('', view_func=review_view, methods=['POST'])
