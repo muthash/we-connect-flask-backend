b8db5e8d5d5b0855204b75fc5dafdc9b55268e4f
Muthash <stephen.kathingo@andela.com>
2018-05-17 12:59:25 +0300

    Add business manipulation endpoints

--- app/business/views.py
+++ app/business/views.py
@@ -2,100 +2,111 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity, jwt_optional
 from app.models import User,  Business
-from app.utils import validate_null, messages, remove_more_spaces
+from app.utils import require_json, check_missing_field, messages, remove_more_spaces, remove_none_fields, filter_business
 from app.base_view import BaseView
 
-biz = Blueprint('biz', __name__, url_prefix='/api/v1/business')
-POSTS_PER_PAGE = 1
+biz = Blueprint('biz', __name__, url_prefix='/api/v1/businesses')
 
 
 class BusinessManipulation(BaseView):
     """Method to manipulate business endpoints"""
     @jwt_required
+    @require_json
     def post(self):
         """Endpoint to save the data to the database"""
-        if not self.invalid_json():
-            data = request.get_json()
-            name = data.get('name')
-            description = data.get('description')
-            category = data.get('category')
-            location = data.get('location')
-            user_id = get_jwt_identity()
+        data = request.get_json()
+        name = data.get('name')
+        description = data.get('description')
+        category = data.get('category')
+        location = data.get('location')
+        user_id = get_jwt_identity()
             
-            data_ = dict(name=name, description=description, category=category, location=location)
-            business_data = validate_null(**data_)
-            if not self.null_input(business_data):
-                user = User.query.filter_by(id=user_id).first()
-                name = remove_more_spaces(name)
-                description = remove_more_spaces(description)
-                category = remove_more_spaces(category)
-                location = remove_more_spaces(location)
-                if user:
-                    business = Business(**data_, user_id=user_id)
-                    business.save()
-                    return self.generate_response(messages['business_created'], 201)
-                return self.generate_response(messages['valid_login'], 403)
-            return self.null_input(business_data)
-        return self.invalid_json()
+        data = dict(name=name, description=description, category=category, location=location)
+        if check_missing_field(**data):
+            return jsonify(check_missing_field(**data)), 422
+                
+        user = User.query.filter_by(id=user_id).first()
+        name = remove_more_spaces(name)
+        description = remove_more_spaces(description)
+
+        if not user:
+            return self.generate_response(messages['valid_login'], 403)
+        business = Business(name, description, category, location, user_id)
+        business.save()
+        return self.generate_response(messages['business_created'], 201)
     
     @jwt_required
+    @require_json
     def put(self, business_id):
         """update a single business"""
-        if not self.invalid_json():
-            data = request.get_json()
-            name = data.get('name')
-            description = data.get('description')
-            category = data.get('category')
-            location = data.get('location')
-            user_id = get_jwt_identity()
+        data = request.get_json()
+        name = data.get('name')
+        description = data.get('description')
+        category = data.get('category')
+        location = data.get('location')
+        user_id = get_jwt_identity()
 
-            data_ = dict(name=name, description=description, category=category, location=location)
-            business_data = validate_null(**data_)
-            if not self.null_input(business_data):
-                business = Business.query.filter_by(id=business_id, user_id=user_id).first()
-                if business:
-                    Business.update(Business, business_id, **data_)
-                    return self.generate_response(messages['business_updated'], 200)
-                return self.generate_response(messages['forbidden'], 403)
-            return self.null_input(business_data)
-        return self.invalid_json()
+        data = dict(name=name, description=description, category=category, location=location)
+        if not remove_none_fields(**data):
+            return self.generate_response(messages['one_update'], 422)
+        
+        update_data = remove_none_fields(**data)
+        business = Business.query.filter_by(id=business_id).first()
+        if not business:
+            return self.generate_response(messages['not_found'], 200)
+        if business.user_id != user_id:
+            return self.generate_response(messages['forbidden'], 403)
+        business.update(business, **update_data)
+        response = {'message': messages['business_updated'],
+                    'business': business.serialize()}
+        return jsonify(response), 200
+
 
     @jwt_required
+    @require_json
     def delete(self, business_id):
         """delete a single business"""
-        if not self.invalid_json():
-            data = request.get_json()
-            password = data.get('password')
-            user_id = get_jwt_identity()
+        data = request.get_json()
+        password = data.get('password')
+        user_id = get_jwt_identity()
 
-            user_data = validate_null(password=password)
-            if not self.null_input(user_data):
-                user = User.query.filter_by(id=user_id).first()
-                if user and user.password_is_valid(password):
-                    business = Business.query.filter_by(id=business_id, user_id=user_id).first()
-                    if business:
-                        business.delete()
-                        return self.generate_response(messages['business_delete'], 200)
-                    return self.generate_response(messages['forbidden'], 403)
-                return self.generate_response(messages['valid_pass'], 401)
-            return self.null_input(user_data)
-        return self.invalid_json()
+        user_data = dict(password=password)
+        if check_missing_field(**user_data):
+            return jsonify(check_missing_field(**user_data)), 422
+        
+        user = User.query.filter_by(id=user_id).first()
+        if not user.password_is_valid(password):
+            return self.generate_response(messages['incorrect'], 401)
+        business = Business.query.filter_by(id=business_id).first()
+        if not business:
+            return self.generate_response(messages['not_found'], 404)
+        if business.user_id != user_id:
+            return self.generate_response(messages['forbidden'], 403)
+        business.delete()
+        return self.generate_response(messages['business_delete'], 200)
+        
+
 
     @jwt_optional
     def get(self, business_id):
         """return a list of all businesses else a single business"""
         if business_id is None:
+            category = request.args.get('cat', "", type=str)
+            location = request.args.get('loc', "", type=str)
             page = request.args.get('page', 1, type=int)
-            page_items = Business.query.paginate(page, POSTS_PER_PAGE, False)
-            page_items_list = [Business.serialize() for Business in page_items.items]
+            limit = request.args.get('limit', 2, type=int)
+
+            page_items = Business.query.paginate(page, limit, False)
+            if category or location:
+                return filter_business(page_items, category=category, location=location)
+            
+            page_items_list = [business.serialize() for business in page_items.items]
             if page_items_list:
                 next_url = page_items.next_num  if page_items.has_next else None
                 prev_url = page_items.prev_num  if page_items.has_prev else None
-                response = {
-                    'message': messages['businesses'],
-                    'businesses': page_items_list,
-                    'next_page': next_url,
-                    'prev_page': prev_url
+                response = {'businesses': page_items_list,
+                            'next_page': next_url,
+                            'prev_page': prev_url
                 }
                 return jsonify(response), 200
             return self.generate_response(messages['no_business'], 404)
@@ -103,11 +114,9 @@ class BusinessManipulation(BaseView):
             business = Business.query.filter_by(id=business_id).first()
             if business:
                 reviews = [business.reviews.serialize() for review in business.reviews]
-                response = {
-                    'message': 'Business found',
-                    'business': business.serialize(),
-                    'number_of_reviews': len(business.reviews),
-                    'reviews': reviews
+                response = {'business': business.serialize(),
+                            'number_of_reviews': len(business.reviews),
+                            'reviews': reviews
                 }
                 return jsonify(response), 200
             return self.generate_response(messages['not_found'], 404)
@@ -117,3 +126,5 @@ business_view = BusinessManipulation.as_view('businesses')
 biz.add_url_rule('', defaults={'business_id':None}, view_func=business_view, methods=['GET',])
 biz.add_url_rule('', view_func=business_view, methods=['POST',])
 biz.add_url_rule('/<int:business_id>', view_func=business_view, methods=['GET', 'PUT', 'DELETE'])
+biz.add_url_rule('/<int:business_id>/reviews', view_func=business_view,
+                 methods=['GET'])
