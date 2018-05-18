b26f1f88f363626ac9788d59733ecc3512ad8907
Muthash <stephen.kathingo@andela.com>
2018-05-18 10:32:50 +0300

    [Feature #156687753] Update auth user functionality

--- app/auth/views.py
+++ app/auth/views.py
@@ -24,13 +24,12 @@ class RegisterUser(BaseView):
         username = data.get('username')
         password = data.get('password')
         user_data = dict(email=email, username=username, password=password)
-        
+
         if check_missing_field(**user_data):
             return jsonify(check_missing_field(**user_data)), 422
         if validate_registration(email, username, password):
             return validate_registration(email, username, password)
-        
-        email = normalise_email(email) 
+        email = normalise_email(email)
         user = User.query.filter_by(email=email).first()
         if user:
             return self.generate_response(messages['exists'], 409)
@@ -57,7 +56,7 @@ class LoginUser(BaseView):
         if user and user.password_is_valid(password):
             return self.generate_token(messages['login'], user.id)
         return self.generate_response(messages['valid_epass'], 401)
-            
+
 
 class LogoutUser(BaseView):
     """Method to logout a user"""
@@ -84,7 +83,7 @@ class ResetPassword(BaseView):
             return jsonify(check_missing_field(**user_data)), 422
         if check_email(email):
             return check_email(email)
-        
+
         email = normalise_email(email)
         user = User.query.filter_by(email=email).first()
         if not user:
@@ -94,7 +93,7 @@ class ResetPassword(BaseView):
         send_reset_password(email, password)
         user.update(user, password=hash_password)
         return self.generate_response(messages['sent_mail'], 201)
-               
+
 
 class ChangePassword(BaseView):
     """Method to change a user password"""
@@ -113,42 +112,46 @@ class ChangePassword(BaseView):
             return jsonify(check_missing_field(**user_data)), 422
         if check_password(new_password):
             return check_password(new_password)
-                
         user = User.query.filter_by(id=user_id).first()
         if user and user.password_is_valid(old_password):
-            hash_password = Bcrypt().generate_password_hash(new_password).decode()                
-            user.update(user, password=hash_password)
+            password = Bcrypt().generate_password_hash(new_password).decode()
+            user.update(user, password=password)
             blacklist = BlacklistToken(token=jti)
             blacklist.save()
             return self.generate_response(messages['password'], 201)
         return self.generate_response(messages['valid_pass'], 401)
 
-# class DeleteAccount(BaseView):
-#     """Method to used to delete a user's account"""
-#     @jwt_required
-#     @require_json
-#     def delete(self):
-#         """Endpoint to change a user password"""
-#         data = request.get_json()
-#         password = data.get('password')
-#         user_id = get_jwt_identity()
-#         jti = get_raw_jwt()['jti']
 
-#         if check_missing_field(**user_data):
-#             return jsonify(check_missing_field(**user_data)), 422
-    
-#         user = User.query.filter_by(id=user_id).first()
-#         if user and user.password_is_valid(password):
-#             user.delete()
-#             blacklist = BlacklistToken(token=jti)
-#             blacklist.save()
-#             return self.generate_response(messages['delete'], 200)
-#         return self.generate_response(messages['valid_pass'], 401)
+class DeleteAccount(BaseView):
+    """Method to used to delete a user's account"""
+    @jwt_required
+    @require_json
+    def delete(self):
+        """Endpoint to delete a user account"""
+        data = request.get_json()
+        password = data.get('password')
+        user_id = get_jwt_identity()
+        jti = get_raw_jwt()['jti']
+
+        user_data = dict(password=password)
+        if check_missing_field(**user_data):
+            return jsonify(check_missing_field(**user_data)), 422
+
+        user = User.query.filter_by(id=user_id).first()
+        if not user.password_is_valid(password):
+            return self.generate_response(messages['valid_pass'], 401)
+        user.delete()
+        blacklist = BlacklistToken(token=jti)
+        blacklist.save()
+        return self.generate_response(messages['delete'], 200)
 
 
 auth.add_url_rule('/register', view_func=RegisterUser.as_view('register'))
 auth.add_url_rule('/login', view_func=LoginUser.as_view('login'))
 auth.add_url_rule('/logout', view_func=LogoutUser.as_view('logout'))
-auth.add_url_rule('/reset-password', view_func=ResetPassword.as_view('reset-password'))
-auth.add_url_rule('/change-password', view_func=ChangePassword.as_view('change-password'))
-# auth.add_url_rule('/delete-account', view_func=DeleteAccount.as_view('delete-account'))
+auth.add_url_rule('/reset-password',
+                  view_func=ResetPassword.as_view('reset-password'))
+auth.add_url_rule('/change-password',
+                  view_func=ChangePassword.as_view('change-password'))
+auth.add_url_rule('/delete-account',
+                  view_func=DeleteAccount.as_view('delete-account'))
