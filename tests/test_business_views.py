932d8250d1fc478f781889b659f2abcc9e006eb0
Muthash <stephen.kathingo@andela.com>
2018-05-18 10:26:32 +0300

    [Feature] Add business functionality tests

--- tests/test_business_views.py
+++ tests/test_business_views.py
@@ -1,13 +1,14 @@
 """Test case for business manipulation view"""
 import json
 from tests.base_test_file import BaseTestCase
+from app import db
 
 
 class TestPostBusiness(BaseTestCase):
     """Test for post business endpoint"""
     def register_business(self, msg, code, key='message'):
         self.automate('/api/v1/businesses', key, data=self.business_data,
-                       code=code, msg=msg)
+                      code=code, msg=msg)
 
     def test_business_creation(self):
         """Test create business works correcty"""
@@ -19,7 +20,7 @@ class TestPostBusiness(BaseTestCase):
         """Test create business empty category"""
         self.business_data['category'] = "  "
         self.register_business(key='category-error', code=422,
-                      msg="The category should not be empty")
+                               msg="The category should not be empty")
 
     def test_not_registered_user(self):
         """Test create business for unregistered user"""
@@ -32,7 +33,7 @@ class TestPutBusiness(BaseTestCase):
     """Test for editing business endpoint"""
     def edit_business(self, msg, code, key='message'):
         self.automate('/api/v1/businesses/1', key, data=self.business_data,
-                       method='put', code=code, msg=msg)
+                      method='put', code=code, msg=msg)
 
     def test_business_can_be_edited(self):
         """Test edit an existing business works as expected"""
@@ -48,8 +49,8 @@ class TestPutBusiness(BaseTestCase):
     def test_edit_not_existing(self):
         """Test update business with no data"""
         self.automate('/api/v1/businesses/10', data=self.business_data,
-                       key='message', method='put', code=200, 
-                       msg="The business was not found")
+                      key='message', method='put', code=200,
+                      msg="The business was not found")
 
     def test_not_own_business(self):
         """Test update not own business"""
@@ -62,7 +63,7 @@ class TestDeleteBusiness(BaseTestCase):
     """Test for delete business endpoint"""
     def delete_business(self, msg, code, key='message'):
         self.automate('/api/v1/businesses/1', key, method='delete',
-                       data=self.reg_data, code=code, msg=msg)
+                      data=self.reg_data, code=code, msg=msg)
 
     def test_business_can_be_deleted(self):
         """Test delete an existing business"""
@@ -76,14 +77,14 @@ class TestDeleteBusiness(BaseTestCase):
 
     def test_incorrect_password(self):
         """Test delete with incorrect password"""
-        self.reg_data['password']='Test12345'
+        self.reg_data['password'] = 'Test12345'
         self.delete_business(code=401, msg='Enter correct password to delete')
 
     def test_not_exist_business(self):
         """Test delete non existing business"""
         self.automate(url='/api/v1/businesses/10', data=self.reg_data,
-                       method='delete', code=404, key='message',
-                       msg='The business was not found')
+                      method='delete', code=404, key='message',
+                      msg='The business was not found')
 
     def test_delete_another_user_business(self):
         """Test delete a business that user did not create"""
@@ -93,3 +94,55 @@ class TestDeleteBusiness(BaseTestCase):
             self.get_login_token(self.reg_data)
             self.delete_business(code=403,
                                  msg='The operation is Forbidden')
+
+
+class TestGetBusiness(BaseTestCase):
+    """Test for get business endpoint"""
+    def get_business(self, url):
+        res = self.client.get(path=url)
+        return json.loads(res.data.decode())
+
+    def test_all_businesses(self):
+        """Test get all registered businesses"""
+        result = self.get_business('/api/v1/businesses')
+        self.assertTrue(result['businesses'])
+
+    def test_empty_businesses(self):
+        """Test get all with no registered businesses"""
+        with self.app.app_context():
+            db.drop_all()
+            db.create_all()
+            result = self.get_business('/api/v1/businesses')
+            self.assertEqual(result['message'],
+                             "There are no businesses registered currently")
+
+    def test_filter_businesses_by_category(self):
+        """Test get all registered businesses in a category"""
+        result = self.get_business('/api/v1/businesses?cat=IT')
+        self.assertTrue(result['businesses'])
+
+    def test_filter_businesses_by_location(self):
+        """Test get all registered businesses in a location"""
+        result = self.get_business('/api/v1/businesses?loc=Nairobi')
+        self.assertTrue(result['businesses'])
+
+    def test_filter_category_and_location(self):
+        """Test get all registered businesses in a category"""
+        result = self.get_business('/api/v1/businesses?cat=IT&loc=Nairobi')
+        self.assertTrue(result['businesses'])
+
+    def test_not_found_category(self):
+        """Test filter not available category"""
+        result = self.get_business('/api/v1/businesses?cat=Farming')
+        self.assertEqual(result['message'],
+                         'The filter criteria did not match any business')
+
+    def test_business_id(self):
+        """Test get single business"""
+        result = self.get_business('/api/v1/businesses/1')
+        self.assertTrue(result['business'])
+
+    def test_invalid_business_id(self):
+        """Test get not available single business"""
+        result = self.get_business('/api/v1/businesses/10')
+        self.assertTrue(result['message'], 'The business 10 is not available')
